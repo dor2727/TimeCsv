019592319a7443ad95eeeba001af50e0426b7ff2
Dor Harpaz <harpazdor@gmail.com>
2020-09-10 17:37:08 +0300

    improved daily_wget logs

--- daily_wget.sh
+++ daily_wget.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-date >> ~/Projects/TimeCsv/daily_wget.log 2>&1
-~/Projects/Dropbox-Uploader/dropbox_uploader.sh download Projects/TimeCsv/data/2020_year_2_big_holiday.tcsv ~/Projects/TimeCsv/data/_latest >> ~/Projects/TimeCsv/daily_wget.log 2>&1
-echo moving >> ~/Projects/TimeCsv/daily_wget.log 2>&1
-mv ~/Projects/TimeCsv/data/_latest ~/Projects/TimeCsv/data/2020_year_2_big_holiday.tcsv >> ~/Projects/TimeCsv/daily_wget.log 2>&1
+date > ~/Projects/TimeCsv/daily_wget.log.new 2>&1
+~/Projects/Dropbox-Uploader/dropbox_uploader.sh download Projects/TimeCsv/data/2020_year_2_big_holiday.tcsv ~/Projects/TimeCsv/data/_latest >> ~/Projects/TimeCsv/daily_wget.log.new 2>&1
+echo moving >> ~/Projects/TimeCsv/daily_wget.log.new 2>&1
+mv ~/Projects/TimeCsv/data/_latest ~/Projects/TimeCsv/data/2020_year_2_big_holiday.tcsv >> ~/Projects/TimeCsv/daily_wget.log.new 2>&1
+cat ~/Projects/TimeCsv/daily_wget.log.new ~/Projects/TimeCsv/daily_wget.log
