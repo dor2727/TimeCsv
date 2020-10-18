380d2f50c1adecbb5485783ed5d950781d31c5f8
Dor Harpaz <harpazdor@gmail.com>
2020-10-18 12:08:39 +0300

    updated wget

--- daily_wget.sh
+++ daily_wget.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-date > ~/Projects/TimeCsv/daily_wget.log.new 2>&1
-~/Projects/Dropbox-Uploader/dropbox_uploader.sh download Projects/TimeCsv/data/2020_year_2_big_holiday.tcsv ~/Projects/TimeCsv/data/_latest >> ~/Projects/TimeCsv/daily_wget.log.new 2>&1
-echo moving >> ~/Projects/TimeCsv/daily_wget.log.new 2>&1
-mv ~/Projects/TimeCsv/data/_latest ~/Projects/TimeCsv/data/2020_year_2_big_holiday.tcsv >> ~/Projects/TimeCsv/daily_wget.log.new 2>&1
-cat ~/Projects/TimeCsv/daily_wget.log.new >> ~/Projects/TimeCsv/daily_wget.log
+PROJECT=TimeCsv
+FOLDER=~/Projects/$PROJECT
+FILENAME="2021_year_3_semester_1.tcsv"
+LOG=$FOLDER/daily_wget.log
+LOG_NEW=$FOLDER/daily_wget.log.new
+TEMP_LOCATION=$FOLDER/data/_latest
+
+date > $LOG_NEW 2>&1
+~/Projects/Dropbox-Uploader/dropbox_uploader.sh download Projects/$PROJECT/data/$FILENAME $TEMP_LOCATION >> $LOG_NEW 2>&1
+echo moving $FILENAME >> $LOG_NEW 2>&1
+mv $TEMP_LOCATION $FOLDER/data/$FILENAME >> $LOG_NEW 2>&1
+cat $LOG_NEW >> $LOG
