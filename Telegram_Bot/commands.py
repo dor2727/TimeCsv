62005587477aba2970ce44eea3c153aa1c931540
Dor Harpaz <harpazdor@gmail.com>
2020-10-20 18:19:01 +0300

    added error handling for the scheduler thread

--- Telegram_Bot/commands.py
+++ Telegram_Bot/commands.py
@@ -458,8 +458,18 @@ class TelegramScheduledCommands(object):
 
 		def run_scheduler():
 			while True:
-				schedule.run_pending()
-				time.sleep(60*60*0.5)
+				try:
+					schedule.run_pending()
+					time.sleep(60*60*0.5)
+
+				except ParseError as pe:
+					log(f"[*] Caught ParseError in run_scheduler. retrying in {RETRY_SLEEP_AMOUNT_IN_HOURS} hours")
+					time.sleep(RETRY_SLEEP_AMOUNT_IN_HOURS * 60 * 60)
+					os.system(DAILY_WGET_PATH)
+
+				except Exception as exc:
+					log(f"[!] Caught general error in run_scheduler - quitting")
+					raise exc
 
 		threading.Thread(target=run_scheduler).start()
 
@@ -483,12 +493,12 @@ def main():
 			t.loop()
 
 		except ParseError as pe:
-			log(f"[*] Caught ParseError. retrying in {RETRY_SLEEP_AMOUNT_IN_HOURS} hours")
+			log(f"[*] Caught ParseError in main. retrying in {RETRY_SLEEP_AMOUNT_IN_HOURS} hours")
 			time.sleep(RETRY_SLEEP_AMOUNT_IN_HOURS * 60 * 60)
 			os.system(DAILY_WGET_PATH)
 
 		except Exception as exc:
-			log(f"[!] Caught general error - quitting")
+			log(f"[!] Caught general error in main - quitting")
 			raise exc
 	LOG_FILE.close()
 
