469f706084c0414c9d3c607cb5219f5013f5bffb
Dor Harpaz <harpazdor@gmail.com>
2020-10-25 22:56:05 +0200

    updated bot interface

--- filters.py
+++ filters.py
@@ -651,3 +651,47 @@ def join_filters_with_and(l):
 		f &= i
 
 	return f
+
+def get_named_filter(name):
+	if name == "today":
+		return TimeFilter_Days(1)
+
+	elif name == "yesterday":
+		stop_time  = get_midnight( datetime.datetime.now() )
+		start_time = get_midnight(
+			stop_time
+			 -
+			datetime.timedelta(days=1)
+		)
+
+		return TimeFilter_DateRange( start_time, stop_time )
+
+	elif name == "week":
+		return TimeFilter_Days(7)
+
+	elif name == "last_week":
+		today = datetime.datetime.now()
+
+		if WEEK_STARTS_AT_SUNDAY:
+			weekday = today.weekday() + WEEK_STARTS_AT_SUNDAY
+			if weekday == 7:
+				weekday = 0
+		else:
+			weekday = today.weekday()
+		this_sunday = get_midnight(today - datetime.timedelta(days=weekday))
+		prev_sunday = this_sunday - datetime.timedelta(days=7)
+
+		return TimeFilter_DateRange( prev_sunday, this_sunday )
+
+	elif name == "month":
+		return TimeFilter_Month()
+
+	elif name == "year":
+		return TimeFilter_Year()
+
+	elif name == "all":
+		return TrueFilter()
+
+	else:
+		return None
+	
