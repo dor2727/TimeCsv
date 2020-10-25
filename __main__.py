7a5e2ebd49f541894279747327e6815b0113830a
Dor Harpaz <harpazdor@gmail.com>
2020-10-25 21:26:43 +0200

    reverted unwanted change

--- __main__.py
+++ __main__.py
@@ -2,7 +2,6 @@ from TimeCsv.cli import main
 from TimeCsv.parsing import DataFolder
 
 if __name__ == '__main__':
-	# d = DataFolder()
-	# print(main(d))
-	print(main(None))
+	d = DataFolder()
+	print(main(d))
 	
