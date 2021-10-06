d9d520d1eec264d9ad0cc52257901d74d4df34f4
Dor Harpaz <harpazdor@gmail.com>
2021-10-06 13:09:02 +0300

    refactored TelegramBot, from TelegramBotTemplate

---
+++ Telegram_Bot/telegram_bot_template.py
@@ -0,0 +1,256 @@
+import time
+import socket
+import logging
+import threading
+import schedule
+
+from TimeCsv.Telegram_Bot.consts import KEY_FILEPATH, CHAT_ID_FILEPATH
+from TimeCsv.Telegram_Bot.wrappers import wrapper_log, wrapper_log_secure, wrapper_whitelist
+from TimeCsv.Telegram_Bot.utils import *
+# from TelegramBots.consts import *
+# from TelegramBots.utils import *
+# from TelegramBots.wrappers import *
+
+# from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
+
+class TelegramServer(object):
+	_wrappers = [wrapper_log]
+	def __init__(self):
+		self.updater = Updater(
+			read_file(KEY_FILEPATH),
+			use_context=True
+		)
+
+	@property
+	def dp(self):
+		return self.updater.dispatcher
+
+	def chat_id(self, update):
+		return get_chat_id(update)
+
+	def send_text(self, text, update, **kwargs):
+		self.updater.bot.sendMessage(
+			self.chat_id(update),
+			text,
+			**kwargs
+		)
+
+	def send_image(self, image_file, update, **kwargs):
+		self.updater.bot.send_photo(
+			self.chat_id(update),
+			photo=image_file,
+			**kwargs
+		)
+
+	def loop(self):
+		logging.info("Entering loop")
+		self.updater.start_polling(timeout=123)
+		self.updater.idle()
+
+	def loop_no_error(self):
+		while True:
+			try:
+				self.loop()
+
+			# socket.gaierror: [Errno -3] Temporary failure in name resolution
+			except socket.gaierror as exc:
+				if exc.errno == -3:
+					logging.warning(f"[*] Caught socket.gaierror (-3) in main (loop) - continue")
+				else:
+					logging.warning(f"[*] Caught socket.gaierror ({exc.errno}) in main (loop) - continue")
+					logging.warning(str(exc))
+
+				continue
+
+			except Exception as exc:
+				logging.warning(f"[!] Caught general error in main (loop) - quitting")
+				logging.warning(exception_message())
+				raise exc
+
+
+class TelegramSecureServer(TelegramServer):
+	# log first, then check the whitelist (so that 'bad' calls are logged)
+	_wrappers = [wrapper_log_secure, wrapper_whitelist]
+
+	# _MAIN_USER may have one of the following values:
+	# 1) None - no main user
+	# 2) a string - the name of the main user
+	# 3) a number - the chat_id of the main user
+	_MAIN_USER = None
+
+	def __init__(self):
+		super().__init__()
+
+		self._get_all_users()
+		self._print_all_users()
+
+	def _get_all_users(self):
+		if os.path.isdir(CHAT_ID_FILEPATH):
+			files = filter(
+				is_chat_id,
+				get_folder_files(CHAT_ID_FILEPATH)
+			)
+		elif os.path.isfile(CHAT_ID_FILEPATH):
+			files = [CHAT_ID_FILEPATH]
+		else:
+			raise ValueError(f"Invalid CHAT_ID_FILEPATH ({CHAT_ID_FILEPATH})")
+
+		self.user_names    = []
+		self.user_chat_ids = []
+
+		for f in files:
+			self.user_names.append(get_chat_id_username(f))
+			self.user_chat_ids.append(get_chat_id_from_file_name(f))
+
+		self.users = dict(zip(self.user_names, self.user_chat_ids))
+
+	def _print_all_users(self):
+		logging.info("Current users:")
+		for i in range(len(self.user_chat_ids)):
+			logging.info(f"    {i}) {self.user_names[i]} - {self.user_chat_ids[i]}")
+
+	@property
+	def MAIN_USER(self):
+		if type(self._MAIN_USER) is int:
+			if self._MAIN_USER in self.user_chat_ids:
+				return self._MAIN_USER
+			else:
+				logging.error(f"MAIN_USER ({self._MAIN_USER}) not in user_chat_ids ({str(self.user_chat_ids)})")
+				raise ValueError("MAIN_USER is not a registered user")
+
+		elif type(self._MAIN_USER) is str:
+			if self._MAIN_USER in self.user_names:
+				return self.users[self._MAIN_USER]
+			else:
+				logging.error(f"MAIN_USER ({self._MAIN_USER}) not in user_names ({str(self.user_names)})")
+				raise ValueError("MAIN_USER is not a registered user")
+
+		elif self._MAIN_USER is None:
+			raise ValueError("MAIN_USER was accessed, yet no MAIN_USER was defined")
+
+		else:
+			logging.error(f"MAIN_USER = {type(self._MAIN_USER)} : {str(self._MAIN_USER)}")
+			raise NotImplemented("An invalid value for self.MAIN_USER was given")
+
+	# changing function declaration - allowing `update` to be None - meaning the MAIN_USER
+	def chat_id(self, update=None):
+		if update:
+			return super().chat_id(update)
+		else:
+			return self.MAIN_USER
+
+	def send_text(self, text, update=None, **kwargs):
+		super().send_text(text, update, **kwargs)
+
+	def send_image(self, image_file, update=None, **kwargs):
+		super().send_image(image_file, update, **kwargs)
+
+
+
+class TelegramCommands(object):
+	def _get_all_command_names(self):
+		return filter(
+			is_command_name,
+			dir(self)
+		)
+	def _get_all_menu_names(self):
+		return filter(
+			is_menu_name,
+			dir(self)
+		)
+
+	def add_all_handlers(self):
+		def wrap(func):
+			func_name = func.__name__
+			# innermost wrapper is the last in the list
+			for w in self._wrappers[::-1]:
+				func = w(func, self=self, func_name=func_name)
+			return func
+
+		# register commands
+		for command_name in self._get_all_command_names():
+			self.dp.add_handler(
+				CommandHandler(
+					strip_command_name(command_name),
+					wrap( getattr(self, command_name) )
+				)
+			)
+		# register menus
+		for menu_name in self._get_all_menu_names():
+			self.dp.add_handler(
+				CallbackQueryHandler(
+					wrap( getattr(self, menu_name) ),
+					pattern=generate_menu_patter(menu_name),
+				)
+			)
+
+	def parse_args(self, context, *expected_types):
+		result = []
+		if context and context.args:
+			try:
+				for i in range(len(context.args)):
+					result.append(expected_types[i](context.args[i]))
+			except Exception as e:
+				self.send_text(
+					f"parse_args error: {e} ; args = {context.args}",
+					# send it to me, not to the user (avoiding information disclosure)
+					self.chat_id()
+				)
+
+		# fill default values, if needed
+		for i in range(len(result), len(expected_types)):
+			result.append(expected_types[i]())
+
+		return result
+
+	# Usefull for setting bot commands in the BotFather
+	def command_list_commands(self, update=None, context=None):
+		self.send_text(
+			'\n'.join(
+				f"{strip_command_name(c)} - {strip_command_name(c)}"
+				for c in self._get_all_command_names()
+			),
+			update
+		)
+
+class TelegramScheduledCommands(object):
+	def schedule_commands(self):
+		self.schedule_hourly()
+		self.schedule_daily()
+		self.schedule_weekly()
+
+	def schedule_hourly(self):
+		pass
+
+	def schedule_daily(self):
+		pass
+
+	def schedule_weekly(self):
+		pass
+
+	def start_scheduler(self):
+		def run_scheduler():
+			while True:
+				try:
+					schedule.run_pending()
+					time.sleep(SCHEDULER_SLEEP_AMOUNT_IN_SECONDS)
+
+				# socket.gaierror: [Errno -3] Temporary failure in name resolution
+				except socket.gaierror as exc:
+					if exc.errno == -3:
+						logging.warning(f"[*] Caught socket.gaierror (-3) in main (loop) - continue")
+					else:
+						logging.warning(f"[*] Caught socket.gaierror ({exc.errno}) in main (loop) - continue")
+						logging.warning(str(exc))
+
+					continue
+
+				except Exception as exc:
+					log(f"[!] Caught general error in run_scheduler - quitting")
+					log(exception_message())
+					raise exc
+
+		threading.Thread(target=run_scheduler).start()
+
--- Telegram_Bot/wrappers.py
+++ Telegram_Bot/wrappers.py
@@ -1,62 +1,55 @@
-import time
-from TimeCsv.utils import log
+"""
+There are 3 wrappers defined here:
+- wrapper_log
+	logs the command, as well as its arguments.
+	This wrapper is used when no security measures (whilelisting users) is used.
+- wrapper_log_secure
+	logs the command, its arguments, and which user called the command
+	This wrapper is used when security measures are used.
+- wrapper_whitelist
+	Verifies user identity before calling the function being wrapped.
+"""
+import logging
+import telegram
 
-
-"""
-A function to return from a wrapper in case the call to the function was forbidden
-"""
-def void(*args, **kwargs):
-	return None
-
-"""
-A small util, getting the 'message' object from telegram's 'update' object
-"""
-def get_message(update):
-	if update["message"] is not None:
-		return update['message']
-	else:
-		return update['callback_query']['message']
+from TelegramBots.utils import *
 
 
 """
 verifies the user calling the function
 - there may be no user, if the call was scheduled
 - if it was called from telegram, then there has to be a user
-	this user must be the same as the only user allowed (this bot is only used by me)
 """
-def whitelisted_command(func):
-	"""
-	requires:
-		self._chat_id - int - my user id
-	"""
+def wrapper_whitelist(func, **wrapper_kwargs):
 	def func_wrapper(*args, **kwargs):
-		self = args[0]
-		if len(args) > 1 and args[1]:
-			update = args[1]
-
-			chat_id = get_message(update)['chat']['id']
-
-			if chat_id == self._chat_id:
-				log(f"[+] whitelist - success - {chat_id}")
-			else:
-				log(f"[-] whitelist - error - {chat_id}")
-				log(str(update))
-				log(str(args))
-				log(str(kwargs))
-				log('\n')
-				return void
-
 		# scheduled command
-		elif "scheduled" in kwargs and kwargs["scheduled"]:
+		if "scheduled" in kwargs and kwargs["scheduled"]:
 			log(f"[*] whitelist - ignored (scheduled)")
-			return func(*args, **kwargs)
+			kwargs.pop("scheduled")
+
+		elif len(args) >= 1 and type(args[0]) is telegram.update.Update:
+			update = args[0]
+			chat_id = get_chat_id(update)
+			# [!] expecting 'self' to be sent in **wrapper_kwargs
+			self = wrapper_kwargs["self"]
+
+			if chat_id in self.user_chat_ids:
+				user_name = self.user_names[self.user_chat_ids.index(chat_id)]
+				logging.info(f"  [+] Whitelist - success - {chat_id} - {user_name}")
+			else:
+				logging.warning(f"  [-] Whitelist - failed - {chat_id}")
+				logging.warning(f"  [-] Whitelist - failed - update: {str(update)}")
+				logging.warning(f"  [-] Whitelist - failed - args: {str(args)}")
+				logging.warning(f"  [-] Whitelist - failed - kwargs: {str(kwargs)}")
+				return None
 
 		else:
-			# unknown. This should not happen
-			log(f"[!] whitelist - got to the forbidden else")
-			log(str(args))
-			log(str(kwargs))
-			return void
+			logging.error(f"  [-] Whitelist - got to the forbidden else")
+			logging.warning(f"  [-] Whitelist - failed - args: {str(args)}")
+			logging.warning(f"  [-] Whitelist - failed - kwargs: {str(kwargs)}")
+			return None
+
+		return func(*args, **kwargs)
 
 	return func_wrapper
 
@@ -64,33 +57,103 @@ def whitelisted_command(func):
 """
 logging the call to the function
 """
-def log_command(func):
+def _get_command_text(update):
+	if update["message"] is not None:
+		command_text = update['message']['text']
+	else:
+		command_text = update['callback_query']['data']
+
+def wrapper_log(func, **wrapper_kwargs):
 	def func_wrapper(*args, **kwargs):
-		# each function is named "command_something"
-		if func.__name__.startswith("command_"):
-			command_name = func.__name__[8:]
+		if "func_name" in wrapper_kwargs:
+			command_name = wrapper_kwargs["func_name"]
 		else:
 			command_name = func.__name__
 
-		if "scheduled" in kwargs:
-			if kwargs["scheduled"]:
-				log(f"    [*] scheduled command - {command_name}\t{time.asctime()}")
-			kwargs.pop("scheduled")
+
+		if "scheduled" in kwargs and kwargs["scheduled"]:
+			logging.info(f"[*] Command - scheduled - {command_name}")
 
 		else:
 			# args[1] is update
-			if len(args) > 1 and args[1]:
-				update = args[1]
-
-				if update["message"] is not None:
-					command_text = update['message']['text']
-				else:
-					command_text = update['callback_query']['data']
+			if len(args) >= 1 and type(args[0]) is telegram.update.Update:
+				command_text = _get_command_text(args[0])
 			else:
 				command_text = "None"
-			log(f"    [*] got command - {command_text}\tcalling {command_name}\t{time.asctime()}")
+
+			logging.info(f"[*] Command ({command_name}) - text: {command_text}")
 
 		return func(*args, **kwargs)
 
 	return func_wrapper
 
+
+def _get_user_name_from_update(update=None, **kwargs):
+	if type(update) is not telegram.update.Update:
+		return "None"
+
+	if "self" not in kwargs:
+		return "None"
+
+	self = kwargs["self"]
+
+	chat_id = get_chat_id(update)
+
+	if chat_id in self.user_chat_ids:
+		return self.user_names[
+			self.user_chat_ids.index(
+				chat_id
+			)
+		]
+	else:
+		try:
+			nickname = update["message"]["chat"]["username"]
+		except:
+			nickname = "<No username found>"
+
+		return f"Unknown user ({chat_id} : {nickname})"
+
+def wrapper_log_secure(func, **wrapper_kwargs):
+	def func_wrapper(*args, **kwargs):
+		if "func_name" in wrapper_kwargs:
+			command_name = wrapper_kwargs["func_name"]
+		else:
+			command_name = func.__name__
+
+
+		if "scheduled" in kwargs and kwargs["scheduled"]:
+			logging.info(f"[*] Command - scheduled - {command_name}")
+
+		else:
+			if len(args) >= 1 and type(args[0]) is telegram.update.Update:
+				command_text = _get_command_text(args[0])
+				user_name = _get_user_name_from_update(args[0], **wrapper_kwargs)
+			else:
+				command_text = "None"
+				user_name = "None"
+
+			logging.info(f"[*] Command ({command_name}) - from user ({user_name}) - text: {command_text}")
+
+		return func(*args, **kwargs)
+
+	return func_wrapper
+
+"""
+When the wrapper is defined as such:
+class SomeClass:
+	@first_wrapper
+	@second_wrapper
+	def function(self, *args, **kwargs):
+		pass
+
+Then 'func_wrapper' gets:
+	args = [self, update]
+	# probably there's also context, but I've never used it in a wrapper, so I'm not sure
+
+However, when the wrappers are defined as:
+for w in self._wrappers[::-1]:
+	func = w(func)
+
+Then 'func_wrapper' gets:
+	args = [update, context]
+"""
