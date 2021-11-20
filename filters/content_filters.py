5a37c69f1594ba79cf359311969f08be9d243770
Dor Harpaz <harpazdor@gmail.com>
2021-11-20 12:23:50 +0200

    Export filter utils to filter_utils

--- filters/content_filters.py
+++ filters/content_filters.py
@@ -2,39 +2,7 @@ import re
 import operator
 
 from TimeCsv.filters.base_filters import Filter
-
-
-def find_string_in_string(string_to_find, string_to_search_in, regex, case_sensitive):
-	if       regex and     case_sensitive:
-		return bool(re.findall(string_to_find, string_to_search_in))
-
-	elif     regex and not case_sensitive:
-		return bool(re.findall(string_to_find, string_to_search_in, re.I))
-
-	elif not regex and     case_sensitive:
-		return string_to_find in string_to_search_in
-
-	elif not regex and not case_sensitive:
-		return string_to_find in string_to_search_in.lower()
-
-def find_string_in_list(string_to_find, list_to_search_in, regex, case_sensitive):
-	if       regex and     case_sensitive:
-		return any([
-			re.findall(string_to_find, string_to_search_in)
-			for string_to_search_in in list_to_search_in
-		])
-
-	elif     regex and not case_sensitive:
-		return any([
-			re.findall(string_to_find, string_to_search_in, re.I)
-			for string_to_search_in in list_to_search_in
-		])
-
-	elif not regex and     case_sensitive:
-		return string_to_find in list_to_search_in
-
-	elif not regex and not case_sensitive:
-		return string_to_find in map(str.lower, list_to_search_in)
+from TimeCsv.filters.filter_utils import find_string_in_string, find_string_in_list
 
 
 # do not use this class directly - it is a meta class
