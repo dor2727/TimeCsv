37206c8ae50bdd9f2ab9fee6d0d34340d2f4e13c
Dor Harpaz <harpazdor@gmail.com>
2022-08-01 00:26:30 +0300

    Prettify text print using pipe-ascii

---
+++ statistics/consts.py
@@ -0,0 +1,32 @@
+BORDERS = [
+	'└',
+	'┘',
+	'┼',
+	'─',
+	'┴',
+	'├',
+	'┤',
+	'┬',
+	'┌',
+	'┐',
+	'│',
+]
+
+BORDER_HORIZONTAL = BORDERS[10]
+BORDER_VERTICAL = BORDERS[3]
+BORDER_SPLIT_RIGHT = BORDERS[5]
+BORDER_SPLIT_END_RIGHT = BORDERS[0]
+BORDER_SPLIT_LEFT = BORDERS[6]
+BORDER_SPLIT_END_LEFT = BORDERS[1]
+BORDER_SPLIT_BOTH = BORDERS[2]
+BORDER_SPLIT_END_BOTH = BORDERS[4]
+
+
+"""
+┐
+├─
+├┬
+│└
+├─
+└─
+"""
