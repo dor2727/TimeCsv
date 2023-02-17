e1eccb4e96040487ce0a7689cde0248221cc7dc9
Dor Harpaz <harpazdor@gmail.com>
2023-02-17 17:51:22 +0200

    Split to functions

--- filters/generic_filters.py
+++ filters/generic_filters.py
@@ -27,39 +27,10 @@ class AutoFilter(Filter):
 	_not_filter_prefix = ('~', '!')
 
 	def __init__(self, string, case_sensitive=None, force_regex=False):
-		string, exclude, regex, friends, location, vehicle = self._preprocess_string(string, force_regex)
+		string, exclude, regex = self._preprocess_string(string, force_regex)
 
-		if friends:
-			self._filter = join_filters_with_or(
-				FriendFilter(friend,
-					case_sensitive=case_sensitive
-				) for friend in friends
-			)
-
-		elif location:
-			self._filter = LocationFilter(location, case_sensitive=case_sensitive)
-
-		elif vehicle:
-			self._filter = VehicleFilter(vehicle, case_sensitive=case_sensitive)
-
-		elif string[0] in ('<', '>'):
-			self._filter = DurationFilter(string)
-
-		elif string.islower():
-			self._filter = DescriptionFilter(string,
-				case_sensitive=case_sensitive, regex=regex
-			)
-
-		elif string[0].isupper():
-			self._filter = GroupFilter(string,
-				case_sensitive=case_sensitive, regex=regex
-			)
-
-		else:
-			self._filter = StrFilter(string,
-				case_sensitive=case_sensitive, regex=regex
-			)
-
+		if not self._try_to_set_filter_special_case(string, regex):
+			self._set_filter_normal_case(string, regex)
 
 		if exclude:
 			self._filter = NotFilter(self._filter)
@@ -79,28 +50,56 @@ class AutoFilter(Filter):
 		else:
 			exclude = False
 
-		# extract friends
-		if regex:
-			friends = False
-		else:
-			friends = DescriptionDetailsParser_Friends.extract_values_from_string(string)
-
-		# extract location
-		if regex:
-			location = False
-		else:
-			location = DescriptionDetailsParser_Location.extract_values_from_string(string)
-
-		# extract vehicle
-		if regex:
-			vehicle = False
-		else:
-			vehicle = DescriptionDetailsParser_Vehicle.extract_values_from_string(string)
-
 		if not string:
 			raise ValueError("AutoFilter got empty string")
 
-		return string, exclude, regex, friends, location, vehicle
+		return string, exclude, regex
+
+	def _try_to_set_filter_special_case(self, string, regex):
+		if string[0] in ('<', '>'):
+			self._filter = DurationFilter(string)
+			return True
+
+		# all of the following cases do not support regex
+		if regex:
+			return False
+
+		friends = DescriptionDetailsParser_Friends.extract_values_from_string(string)
+		if friends:
+			self._filter = join_filters_with_or(
+				FriendFilter(friend,
+					case_sensitive=case_sensitive
+				) for friend in friends
+			)
+			return True
+
+		location = DescriptionDetailsParser_Location.extract_values_from_string(string)
+		if location:
+			self._filter = LocationFilter(location, case_sensitive=case_sensitive)
+			return True
+
+		vehicle = DescriptionDetailsParser_Vehicle.extract_values_from_string(string)
+		if vehicle:
+			self._filter = VehicleFilter(vehicle, case_sensitive=case_sensitive)
+			return True
+
+		return False
+
+	def _set_filter_normal_case(self, string, regex):
+		if string.islower():
+			self._filter = DescriptionFilter(string,
+				case_sensitive=case_sensitive, regex=regex
+			)
+
+		elif string[0].isupper():
+			self._filter = GroupFilter(string,
+				case_sensitive=case_sensitive, regex=regex
+			)
+
+		else:
+			self._filter = StrFilter(string,
+				case_sensitive=case_sensitive, regex=regex
+			)
 
 
 class AutoTimeFilter(BaseTimeFilter):
