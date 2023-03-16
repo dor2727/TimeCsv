c3c222dd927d645b1d66fcc73b0495c91fe8fa3b
Dor Harpaz <harpazdor@gmail.com>
2023-03-16 19:38:58 +0200

    Add time filters to CLI

--- cli/cli.py
+++ cli/cli.py
@@ -13,12 +13,12 @@ def parse_args(args_list=None):
 
 	# filter time
 	time = parser.add_argument_group("Time")
-	# time.add_argument("--days-back", "-d", type=int , default=None, dest="days_back"   , help="how many days back to query")
-	# time.add_argument("--months-back"    , type=int , default=None, dest="months_back" , help="how many month summaries to show")
-	# time.add_argument("--month"    , "-m", type=str , default=None, dest="month"       , help="which month to query. can be comma seperated (e.g. 1,2,3,9)")
-	# time.add_argument("--year"     , "-y", type=str , default=None, dest="year"        , help="which year to query. can be comma seperated (e.g. 2019,2020)")
-	# time.add_argument("--all-time"       , action="store_true"    , dest="all_time"    , help="whether to all the time")
-	# time.add_argument("--time-use-and"   , action="store_true"    , dest="time_use_and", help="whether to use AND or OR when adding the filters")
+	time.add_argument("--days-back", "-d", type=int , default=None, dest="days_back"   , help="how many days back to query (0 for today only, 1 includes yesterday, etc.)")
+	time.add_argument("--months-back"    , type=int , default=None, dest="months_back" , help="how many month summaries to show")
+	time.add_argument("--month"    , "-m", type=str , default=None, dest="month"       , help="which month to query. can be comma seperated (e.g. 1,2,3,9)")
+	time.add_argument("--year"     , "-y", type=str , default=None, dest="year"        , help="which year to query. can be comma seperated (e.g. 2019,2020)")
+	time.add_argument("--all-time"       , action="store_true"    , dest="all_time"    , help="whether to all the time")
+	time.add_argument("--time-use-and"   , action="store_true"    , dest="time_use_and", help="whether to use AND or OR when adding the filters (default - OR)")
 
 	# filter
 	search = parser.add_argument_group("Search")
@@ -50,7 +50,7 @@ def parse_args(args_list=None):
 
 	return post_process_args(args)
 
-def post_process_args(args):
+def post_process_args(args: argparse.Namespace):
 	if args.pie:
 		args.output = Outputs.Pie
 
